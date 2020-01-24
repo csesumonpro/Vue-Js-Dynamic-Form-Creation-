131cccad54530d2e679fe33637678a3c93ceb98c
csesumonpro <csesumonpro@gmail.com>
2020-01-25 04:40:47 +0600

    vue dynamic form creation complete

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
-import App from './App.vue'
+import { BootstrapVue } from 'bootstrap-vue'
+Vue.use(BootstrapVue)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import MainComponent from './components/MainComponent.vue'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
+  render: h => h(MainComponent),
+
 }).$mount('#app')
+
